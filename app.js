45223abebbc67727731f213671a14a62d5f96c9f
LHSang <lhsang64.contact@gmail.com>
2023-11-13 15:36:19 +0700

    Sang - done setting up

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require("express");
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
-const port = 3000;
+
+app.use("/static", express.static("static"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/test", (req, res) => {
+  res.sendFile(_dirname + "/test.html");
 });
+
+function serverStartedHandler() {
+  console.log("Web server is running at http://localhost:3000");
+}
+
+app.listen(3000, serverStartedHandler);
