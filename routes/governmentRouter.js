d8cc52fce48cfd48fb27834de2d693aaed6f9f36
HuyBean <nqhuy0711@gmail.com>
2023-12-02 18:47:15 +0700

    Huy - successfully write data into map

--- routes/governmentRouter.js
+++ routes/governmentRouter.js
@@ -1,9 +1,29 @@
 import express from "express";
+import positionService from "../services/position.service.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("GET request to /");
-  res.render("governmentPage", { title: "Government Page" });
+router.get('/', async function (req, res) {
+  try {
+    const list = await positionService.findAll();
+    const coordinatesList = list.map(item => [item.Lng, item.Lat]); // Lấy tọa độ từ danh sách dữ liệu
+    
+    // Lấy thông tin chi tiết của từng vị trí trong danh sách
+    const positionInfoPromises = list.map(item => positionService.findById(item.Id));
+    const positionInfo = await Promise.all(positionInfoPromises);
+
+    res.render('governmentPage', {
+      list: list,
+      empty: list.length === 0,
+      coordinatesList: JSON.stringify(coordinatesList), // Truyền danh sách tọa độ vào handlebars
+      positionInfo: JSON.stringify(positionInfo),
+    });
+  } catch (error) {
+    // Xử lý lỗi nếu cần
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
+
 
 export default router;
