32d8f0b7ef0346710cb56130b198a09c45f32fa8
HuyBean <nqhuy0711@gmail.com>
2023-12-03 15:25:40 +0700

    Huy - edit public page

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
+// LIBRARY
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import expbs from "express-handlebars";
 import path from "path";
 
+// PAGES
 import governmentRouter from "./routes/governmentRouter.js";
 import aboutRouter from "./routes/aboutRouter.js";
+import publicRouter from "./routes/publicRouter.js"
 
+// DIRNAME
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
+// HANDLEBARS
 const hbs = expbs.create({
   defaultLayout: "main",
   layoutsDir: path.join(__dirname, "views/layouts"), // change layout folder name
   partialsDir: path.join(__dirname, "views/pieces"), // change partials folder name
 });
 
-
+// SETTING ENGINE
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
 app.use("/static", express.static("static"));
-app.get("/", governmentRouter);
+app.get("/", publicRouter);
+app.get("/government", governmentRouter);
 app.get("/about", aboutRouter);
 
+
+// START 
 function serverStartedHandler() {
   console.log("Web server is running at http://localhost:3000");
 }
