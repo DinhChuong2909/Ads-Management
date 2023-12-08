efaf2430077f2e444ce8aac04a084c8a4c926e2c
LHSang <lhsang64.contact@gmail.com>
2023-12-09 00:36:11 +0700

    Sang - authen

--- app.js
+++ app.js
@@ -5,20 +5,36 @@ import { fileURLToPath } from "url";
 import expbs from "express-handlebars";
 import passport from "passport";
 import path from "path";
-import bodyParser from "body-parser";
 import flash from "connect-flash";
+import methodOverride from "method-override";
+import session from "express-session";
+import bcrypt from "bcryptjs";
+
+// UTILS
+import initializePassport from "./utils/passport.js";
+import { checkAuthenticated, checkNotAuthenticated } from "./utils/passport.js";
 
 // PAGES
 import governmentRouter from "./routes/governmentRouter.js";
 import aboutRouter from "./routes/aboutRouter.js";
-import phuongRouter from "./routes/phuongRouter.js"
-import publicRouter from "./routes/people/homeRouter.js"
+import phuongRouter from "./routes/phuongRouter.js";
+// import publicRouter from "./routes/people/homeRouter.js"
 
 // DIRNAME
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+
+// USER DB (TEMP)
+const users = [];
+
+// INIT PASSPORT
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 // HANDLEBARS
 const hbs = expbs.create({
@@ -27,43 +43,75 @@ const hbs = expbs.create({
   partialsDir: path.join(__dirname, "views/pieces"), // change partials folder name
 });
 
-// SETTING PASSPORT)
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-
 // SETTING ENGINE
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+app.use(
+  session({
+    secret: "SangChuongHuyKTPM2AdsMng",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
 
-// AUTHORIZATION
-// router.get("/", function (req, res, next) {
-//   if (req.isAuthenticated() && "ADMIN" == req.session.passport.user.name_role) {
-//     console.log(req.session.passport.user);
-//     res.render("shop/addProduct");
-//   } else {
-//     res.send("ban da khong co quyen");
-//   }
-// });
+// CHECK AUTHENTICATED
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("phuong", { name: req.user.name });
+});
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("authentication/signIn");
+});
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/phuong",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("authentication/register");
+});
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+  try {
+    console.log("req body:", req.body);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+      dateOfBirth: req.body.dob,
+      phone: phone,
+      role: role,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    console.log("post failed", error);
+    res.redirect("/register");
+  }
+  console.log("Users: ", users);
+});
 
-// canBoApp.use(
-//   session({
-//     secret: "",
-//     resave: false, //don't save session if unmodified
-//     saveUninitialized: true, // don't create session until something stored
-//     store: MongoStore.create({
-//       mongoUrl: mongoURI,
-//     }),
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 24 * 7,
-//     },
-//   }),
-//   cookieParser(""),
-//   flash()
-// );
+app.delete("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).send("Error logging out");
+    }
+    res.redirect("/login");
+  });
+});
+
+// PAGE ROUTERS
 
 app.use("/static", express.static("static"));
 // app.get("/", publicRouter);
