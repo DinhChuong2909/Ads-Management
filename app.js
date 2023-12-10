3ccca0e715253d9dc78099f8c8dcf09228222dad
LHSang <lhsang64.contact@gmail.com>
2023-12-11 01:38:02 +0700

    Sang - fixed local-strategy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 import expbs from "express-handlebars";
 import passport from "passport";
 import path from "path";
-import moment from 'moment'
+import moment from "moment";
 
 import flash from "connect-flash";
 import methodOverride from "method-override";
@@ -30,18 +30,18 @@ const hbs = expbs.create({
   layoutsDir: path.join(__dirname, "views/layouts"), // change layout folder name
   partialsDir: path.join(__dirname, "views/pieces"), // change partials folder name
   helpers: {
-    formatDate: function(datetime, format) {
-        if (moment) {
-            format = DateFormats[format] || format;
-            return moment(datetime).format(format);
-        } else {
-            return datetime;
-        }
+    formatDate: function (datetime, format) {
+      if (moment) {
+        format = DateFormats[format] || format;
+        return moment(datetime).format(format);
+      } else {
+        return datetime;
+      }
     },
     eq: function (a, b, options) {
       return a === b ? options.fn(this) : options.inverse(this);
     },
-  }
+  },
 });
 
 // SETTING ENGINE
@@ -50,17 +50,17 @@ app.set("view engine", "handlebars");
 app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 
-app.use(flash());
 app.use(
   session({
     secret: "SangChuongHuyKTPM2AdsMng",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {},
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 app.use(methodOverride("_method"));
 
 // CHECK AUTHENTICATED
--- utils/passport.js
+++ utils/passport.js
@@ -1,5 +1,6 @@
 import LocalStrategy from "passport-local";
 import bcrypt from "bcryptjs";
+import authenticationService from "../services/authentication.service.js";
 
 function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
@@ -20,6 +21,7 @@ function initialize(passport, getUserByEmail, getUserById) {
   };
 
   passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+
   passport.serializeUser((user, done) => done(null, user.id));
   passport.deserializeUser((id, done) => {
     return done(null, getUserById(id));
