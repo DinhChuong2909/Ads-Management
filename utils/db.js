4a138cd04d9b3e2546bfce24d9ace031c594c228
LHSang <lhsang64.contact@gmail.com>
2024-01-10 10:58:16 +0700

    Sang - added Db and Server deployment

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,35 @@
+// import knexObj from 'knex'
+
+// const knex = knexObj({
+//   client: 'mysql2',
+//   connection: {
+//     host: '127.0.0.1',
+//     port: 3306,
+//     // port: 8889,
+//     user: 'root',
+//     password:"",
+//     // password: 'root',
+//     database: 'qlqc',
+//   },
+//   pool: { min: 0, max: 7 },
+// })
+
+// export default knex
+
 import knexObj from 'knex'
 
 const knex = knexObj({
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    port: 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     // port: 8889,
-    user: 'root',
-    password:"",
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
     // password: 'root',
-    database: 'qlqc',
+    connectionlimit: 10,
+    queueLimit: 0,
+    waitForConnections: true,
   },
   pool: { min: 0, max: 7 },
 })
