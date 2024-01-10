b2d0f874865341c027b50b549e7322556746a77e
LHSang <lhsang64.contact@gmail.com>
2024-01-10 11:05:37 +0700

    Sang - added Db and Server deployment

--- utils/db.js
+++ utils/db.js
@@ -22,9 +22,9 @@ const knex = knexObj({
   client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
+    database: process.env.DB_NAME,
     // port: 8889,
-    user: process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     // password: 'root',
     connectionlimit: 10,
